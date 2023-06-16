4552ccca35882784a262fd0d2f8fe1d86abae29b
hermella <hermellahaddush@gmail.com>
2023-06-16 04:00:05 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - A progra that ptint a line using printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
